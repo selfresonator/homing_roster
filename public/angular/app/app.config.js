be60de1829c34dcd0d93b2560620502f914df48f
selfresonator <selfresonator@gmail.com>
2017-03-19 14:36:07 -0700

    Tests styling development with Grunt automation

--- public/angular/app/app.config.js
+++ public/angular/app/app.config.js
@@ -3,33 +3,13 @@
 
   angular
     .module('app.config')
-    .config(config).config(formProvider);
+    .config(config);
 
   config.$inject = ['$locationProvider'];
 
   function config($locationProvider) {
     //$locationProvider.html5Mode(true);
     //$locationProvider.hashPrefix( '!' );
-  }
-
-  formProvider.$inject = ['$httpProvider'];
-
-  function formProvider($httpProvider) {
-    // Intercept POST requests, convert to standard form encoding for ease
-    $httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
-    $httpProvider.defaults.headers.put["Content-Type"] = "application/x-www-form-urlencoded";
-    $httpProvider.defaults.transformRequest.unshift(function (data, headersGetter) {
-      var key, result = [];
-
-      if (typeof data === "string")
-        return data;
-
-      for (key in data) {
-        if (data.hasOwnProperty(key))
-          result.push(encodeURIComponent(key) + "=" + encodeURIComponent(data[key]));
-      }
-      return result.join("&");
-    });
   }
 
 })();
