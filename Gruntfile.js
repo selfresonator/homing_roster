fbcefec579fdff874f913723d03090df2f7510cf
selfresonator <selfresonator@gmail.com>
2017-03-19 14:33:58 -0700

    Implements Grunt automation for styling development

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-nodemon');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -18,23 +19,22 @@ module.exports = function(grunt) {
           'public/angular/app/links/*.html',
           'Gruntfile.js'
         ]
+      },
+      clientStyles: {
+        files: 'public/scss/*.scss',
       }
     },
 
-    // concat: {
-    //   dist: {
-    //     src: [
-    //       'bower_components/angular/angular.min.js',
-    //       'bower_components/angular-route/angular-route.min.js',
-    //       'public/angular/app/app.js',
-    //       'public/angular/app/app.config.js',
-    //       'public/angular/app/app.routes.js',
-    //       'public/angular/app/app.constants.js',
-    //       'public/angular/app/links/links.controller.js'
-    //     ],
-    //     dest: 'public/angular/app/dist/build.js'
-    //   }
-    // },
+    compass: {
+      dev: {
+        options: {
+          sassDir: 'public/scss',
+          cssDir: 'public/angular/app/styles',
+          environment: 'development',
+          watch: true
+        },
+      }
+    },
 
     nodemon: {
       dev: {
@@ -46,11 +46,6 @@ module.exports = function(grunt) {
       }
     }
   });
-
-  // grunt.registerTask('build', [
-  //   'concat:dist',
-  //   // 'concat:jsmodules'
-  // ]);
 
   grunt.registerTask('spy', ['watch']);
   grunt.registerTask('default', ['nodemon:dev']);
