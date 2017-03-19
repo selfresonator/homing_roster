b566be3ed04734108fa125f376901587bc70ec3a
selfresonator <selfresonator@gmail.com>
2017-03-19 02:34:10 -0700

    Installs Grunt live reload automation; better than webpack; fuck webpack

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,41 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      clientViews: {
+        files: [
+          'public/angular/app/*.js',
+          'public/angular/app/*.html',
+          'public/angular/app/links/*.js',
+          'public/angular/app/links/*.html',
+          'Gruntfile.js'
+        ]
+      }
+    },
+
+    // concat: {
+    //   dist: {
+    //     src: [
+    //       'bower_components/angular/angular.min.js',
+    //       'bower_components/angular-route/angular-route.min.js',
+    //       'public/angular/app/app.js',
+    //       'public/angular/app/app.config.js',
+    //       'public/angular/app/app.routes.js',
+    //       'public/angular/app/app.constants.js',
+    //       'public/angular/app/links/links.controller.js'
+    //     ],
+    //     dest: 'public/angular/app/dist/build.js'
+    //   }
+    // },
+
     nodemon: {
       dev: {
         script: 'app.js',
@@ -14,5 +47,11 @@ module.exports = function(grunt) {
     }
   });
 
+  // grunt.registerTask('build', [
+  //   'concat:dist',
+  //   // 'concat:jsmodules'
+  // ]);
+
+  grunt.registerTask('spy', ['watch']);
   grunt.registerTask('default', ['nodemon:dev']);
 };
