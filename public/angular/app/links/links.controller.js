04edd900426d58e0bbb3999d09d118aab6216d9f
selfresonator <selfresonator@gmail.com>
2017-03-20 00:25:28 -0700

    Basic landing page and card setup

--- public/angular/app/links/links.controller.js
+++ public/angular/app/links/links.controller.js
@@ -4,28 +4,15 @@
   angular
     .module('links.controller')
     .controller('LinksController', LinksController)
-    .directive('linksDirective', LinksDirective);
 
     LinksController.$inject = ['$log'];
 
     function LinksController($log) {
       var vm = this;
       vm.title = 'Social';
+      vm.badges = {};
+
       $log.info('hello');
-    }
-
-    function LinksDirective() {
-      var directive = {
-        link: link,
-        restrict: 'EA',
-        template: '<div style="font-family: Allerta">D3 YO!</div>',
-      }
-
-      return directive
-
-      function link(scope,elem,attrs) {
-        console.log(elem);
-      }
     }
 
 })();
