a858063168a59a38cc72ff31b5db33dd2f1337b2
selfresonator <selfresonator@gmail.com>
2017-03-22 20:35:02 -0700

    Completes dynamic font styles

--- public/angular/app/links/links.controller.js
+++ public/angular/app/links/links.controller.js
@@ -4,69 +4,60 @@
   angular
     .module('links.controller')
     .controller('LinksController', LinksController)
-    .directive('linksDirective', LinksDirective);
 
     LinksController.$inject = ['$log'];
 
     function LinksController($log) {
       var vm = this;
+      vm.hovered = '';
+      vm.color = '';
       vm.bg = true;
       vm.email = false;
       vm.github = false;
       vm.soundcloud = false;
       vm.twitter = false;
       vm.linkedin = false;
-      vm.drive = false;
+      vm.car = false;
+
+      vm.githubhv = githubhv;
+      vm.twitterhv = twitterhv;
+      vm.soundcloudhv = soundcloudhv;
+      vm.linkedinhv = linkedinhv;
+      vm.carhv = carhv;
+      vm.emailhv = emailhv;
+
+      function githubhv() {
+          vm.github = true;
+          vm.color = 'red';
+          vm.bg = false;
+      }
+      function twitterhv() {
+          vm.twitter = true;
+          vm.color = '#00aced';
+          vm.bg = false;
+      }
+      function soundcloudhv() {
+          vm.soundcloud = true;
+          vm.color = '#ff3a00';
+          vm.bg = false;
+      }
+      function linkedinhv() {
+          vm.linkedin = true;
+          vm.color = 'green';
+          vm.bg = false;
+      }
+      function carhv() {
+          vm.car = true;
+          vm.color = 'gold';
+          vm.bg = false;
+      }
+      function emailhv() {
+          vm.email = true;
+          vm.color = 'tan';
+          vm.bg = false;
+      }
 
       $log.info('hello');
     }
 
-    function LinksDirective() {
-      var directive = {
-        link: link,
-        restrict: 'EA',
-        template: '<div style="font-family: Allerta">D3 YO!</div>',
-      }
-
-      return directive
-
-      function link(scope,elem,attrs) {
-        console.log(elem);
-      }
-    }
-
 })();
-
-
-// vm.data = [
-//   {
-//     name: 'github',
-//     url: 'https://github.com/selfresonator',
-//     class: 'fa-github'
-//   },
-//   {
-//     name: 'twitter',
-//     url: 'https://twitter.com/_selfresonator',
-//     class: 'fa-twiiter'
-//   },
-//   {
-//     name: 'soundcloud',
-//     url: 'https://soundcloud.com/selfresonator',
-//     class: ''
-//   },
-//   {
-//     name: 'instagram',
-//     url: '',
-//     class: ''
-//   },
-//   {
-//     name: 'linkedin',
-//     url: 'https://www.linkedin.com/in/ajfarleyengineer/',
-//     class: ''
-//   },
-//   {
-//     name: 'car',
-//     url: '',
-//     class: ''
-//   },
-// ];
